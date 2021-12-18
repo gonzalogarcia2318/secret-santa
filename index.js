68be700d63f9b94828205abfb9dab29b0d1ae16c
gonzalogarcia2318 <gonzalogarcia2318@gmail.com>
2021-12-18 17:09:13 -0300

    Fix in load page.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.onload = function () {
 
     startDatabaseConnection(getDatabaseNameByEvent(eventName));
     //
-    fillResultsList();
+    // fillResultsList();
 }
 
 let participants = [];
