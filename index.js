f38fa3e9ba015dd79a15dbd0f65d6673570689b9
gonzalogarcia2318 <gonzalogarcia2318@gmail.com>
2021-12-18 17:01:51 -0300

    Removed generateData and dev methods.

--- index.js
+++ index.js
@@ -96,19 +96,6 @@ function getInvisibleFriend() {
     }
 }
 
-// Remove all data and then generate test objects.
-function generateData() {
-    database.remove();
-
-    // Nombres de los participantes
-    const names = ["Daniel", "Susana", "Mariana", "Agustina", "Gonzalo"];
-
-    names.forEach(name => {
-        let participant = new Participant(null, name, "", false);
-        save(participant);
-    })
-}
-
 
 function save(participant) {
     database.push().set(participant)
@@ -132,59 +119,4 @@ function update(participant, friend) {
 function getDatabaseNameByEvent(event) {
     // event: NOCHEBUENA | NAVIDAD
     return event == 'NOCHEBUENA' ? 'nochebuenaDb' : 'navidadDb';
-}
-
-
-// Functions to help in the development. Will be removed 
-// 
-//
-function showActions() {
-    let containerDisplay = getComputedStyle(document.getElementById("dataContainer")).display;
-    document.getElementById("dataContainer").style.display = containerDisplay == 'none' || containerDisplay == "" ? 'flex' : 'none';
-}
-
-function toggleElement(elementId) {
-    document.getElementById(elementId).style.opacity = document.getElementById(elementId).style.opacity == "" || document.getElementById(elementId).style.opacity == 1 ? 0 : 1;
-}
-
-function fillResultsList() {
-    database.once('value', function (snapshot) {
-        fillTable("resultsDataTable", snapshot.val());
-    });
-}
-
-function fillList(listId, json) {
-    let items = parseJsonToList(json);
-    //
-    let list = document.getElementById(listId);
-    list.innerHTML = "";
-
-    items.forEach(item => {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = `${item.name} - Friend: ${item.friend} - Selected: ${item.isSelected}`;
-        list.appendChild(listItem);
-    })
-}
-
-function fillTable(tableId, json) {
-    const items = parseJsonToList(json);
-    //
-    const table = document.getElementById(tableId);
-    //
-    items.forEach(item => {
-        let row = table.insertRow();
-        let nameColumn = row.insertCell(0);
-        nameColumn.innerHTML = item.name;
-        let friendColumn = row.insertCell(1);
-        friendColumn.innerHTML = item.friend;
-        //
-        if (item.friend != null && item.friend != "" && item.isSelected) {
-            row.classList.add('completed');
-        } else if (item.friend != null && item.friend != "" && !item.isSelected) {
-            row.classList.add('selected');
-        } else if (item.isSelected) {
-            row.classList.add('chosen');
-        }
-
-    });
 }
