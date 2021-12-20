65751a1dda911615d4643a928c234acecb915da2
gonzalogarcia2318 <gonzalogarcia2318@gmail.com>
2021-12-20 16:35:35 -0300

    Create function to assign every invisible friend. Now the user will only view who is his friend but it doesn't calcule it in the moment.

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
     const eventName = urlParams.get('evento');
     if (eventName) {
-        startDatabaseConnection(getDatabaseNameByEvent(eventName));
+        // startDatabaseConnection(getDatabaseNameByEvent(eventName));
+        startDatabaseConnection('test');
+        refreshParticipants(null);
     }
     //
-    // fillResultsList();
+    fillResultsList();
 }
 
 let participants = [];
@@ -25,16 +27,17 @@ function startDatabaseConnection(dbName) {
 
     database = firebase.database().ref(dbName);
 
-    database.on('value', function (snapshot) {
-        console.warn("Database was updated. Refresh data.")
-        parseData(snapshot.val());
-    });
+    // Subscribe to db changes. No need if we are only showing the pre-calculated results.
+    // database.on('value', function (snapshot) {
+    //     console.warn("Database was updated. Refresh data.")
+    //     parseData(snapshot.val());
+    // });
 }
 
 function parseJsonToList(json) {
     let list = [];
     for (key in json) {
-        let participant = new Participant(key, json[key].name, json[key].friend, json[key].isSelected);
+        let participant = new Participant(key, json[key].name, json[key].friend, json[key].isSelected, json[key].friendViewed);
         list.push(participant);
     }
     return list;
@@ -53,8 +56,8 @@ function parseData(json) {
     participantsSelect.add(option);
 
     participants.forEach(participant => {
-        if (participant.friend == "" || participant.friend == null) {
-            let option = document.createElement('option');
+        if (!participant.friendViewed) {
+            const option = document.createElement('option');
             option.text = participant.name;
             option.value = participant.id;
             participantsSelect.add(option);
@@ -64,64 +67,57 @@ function parseData(json) {
     participantsSelect.disabled = false;
 }
 
-function getInvisibleFriend() {
-    if (participantsSelect.value != null && participantsSelect.value != "") {
-        let participant = participants.find(p => p.id == participantsSelect.value);
-        let invisibleFriend;
+function getInvisibleFriend(participant) {
+    let invisibleFriend;
+    //
+    if (participant != null) {
+        const possibleFriends = participants.filter(p => participant.id != p.id && !p.isSelected);
         //
-        if (participant != null) {
-            //
-            console.log("Getting invisible friend for: ", participant);
-            //
-            const possibleFriends = participants.filter(p => participant.id != p.id && !p.isSelected && p.name != 'Flavio' && p.name != 'Elsa');
-            //
-            const randomIndex = Math.floor(Math.random() * possibleFriends.length);
-            invisibleFriend = possibleFriends[randomIndex];
-
-            //
-            let specialCase = false;
-            if(participant.name == 'Miriam' || participant.name == 'Elsa'){
-                specialCase = true;
-                invisibleFriend = handleSpecialCases(participant, participants);
-            }
-            
-
-            // Validation for the last participant
-            // The last participant could get himself as friend, so we have to check in the previous one. 
-            if (possibleFriends.length == 2 && !specialCase) {
-                invisibleFriend.isSelected = true;
-                possibleFriends.splice(possibleFriends.indexOf(invisibleFriend), 1);
-
-                const remainingParticipant = possibleFriends[0];
-
-                if (!remainingParticipant.friend) {
-                    const possibleFriendsForLastOne = participants.filter(p => remainingParticipant.id != p.id && !p.isSelected);
-
-                    if (possibleFriendsForLastOne.length == 0) {
-                        // Last partipant would get himselft as friend, so we have to switch them.
-                        invisibleFriend.isSelected = false;
-                        invisibleFriend = remainingParticipant;
-                    }
-                }
-
-            }
-
-            //
+        const randomIndex = Math.floor(Math.random() * possibleFriends.length);
+        invisibleFriend = possibleFriends[randomIndex];
+        //
+        // Validation for the last participant
+        // The last participant could get himself as friend, so we have to check in the previous one. 
+        if (possibleFriends.length == 2) {
             invisibleFriend.isSelected = true;
-            participant.friend = invisibleFriend.name;
-            //
-            resultLabelText = document.getElementById("resultLabelText");
-            resultLabelText.innerHTML = `Tu amigo invisible es: ${invisibleFriend.name.toUpperCase()}`
-            //
-            document.getElementById('resultLabel').classList.add('fade-in--show');
-            document.getElementById('resultLabel').classList.remove('fade-in--hide');
-            //
-            document.getElementById("pickerContainer").classList.add('fade-out--hide');
-            document.getElementById("pickerContainer").classList.add('fade-out--show');
-            document.getElementById("pickerContainer").style.pointerEvents = 'none';
+            possibleFriends.splice(possibleFriends.indexOf(invisibleFriend), 1);
+
+            const remainingParticipant = possibleFriends[0];
+
+            if (!remainingParticipant.friend) {
+                const possibleFriendsForLastOne = participants.filter(p => remainingParticipant.id != p.id && !p.isSelected);
+
+                if (possibleFriendsForLastOne.length == 0) {
+                    // Last partipant would get himselft as friend, so we have to switch them.
+                    invisibleFriend.isSelected = false;
+                    invisibleFriend = remainingParticipant;
+                }
+            }
+
         }
         //
-        update(participant, invisibleFriend);
+        invisibleFriend.isSelected = true;
+        participant.friend = invisibleFriend.name;
+    }
+    //
+    updateParticipants([participant, invisibleFriend]);
+}
+
+function showInvisibleFriend() {
+    if (participantsSelect.value != null && participantsSelect.value != "") {
+        const participant = participants.find(p => p.id == participantsSelect.value);
+        resultLabelText = document.getElementById("resultLabelText");
+        resultLabelText.innerHTML = `Tu amigo invisible es: ${participant.friend.toUpperCase()}`
+        //
+        document.getElementById('resultLabel').classList.add('fade-in--show');
+        document.getElementById('resultLabel').classList.remove('fade-in--hide');
+        //
+        document.getElementById("pickerContainer").classList.add('fade-out--hide');
+        document.getElementById("pickerContainer").classList.add('fade-out--show');
+        document.getElementById("pickerContainer").style.pointerEvents = 'none';
+        //
+        participant.friendViewed = true;
+        updateParticipant(participant);
     }
 }
 
@@ -136,11 +132,15 @@ function save(participant) {
 }
 
 // Update both participant and it's friend in the database with one call.
-function update(participant, friend) {
+function updateParticipants(participants) {
+    let updates = {};
+    participants.forEach(p => updates[p.id] = p);
+    return database.update(updates);
+}
+
+function updateParticipant(participant) {
     let updates = {};
     updates[participant.id] = participant;
-    updates[friend.id] = friend;
-    //
     return database.update(updates);
 }
 
@@ -151,14 +151,92 @@ function getDatabaseNameByEvent(event) {
 }
 
 
-function handleSpecialCases(participant, participants){
-    let invisibleFriend;
-    if(participant.name == 'Miriam'){
-        invisibleFriend = participants.find(participant => participant.name == 'Elsa');
-    }
-    if(participant.name == 'Elsa'){
-        invisibleFriend = participants.find(participant => participant.name == 'Flavio');
-    }
-    return invisibleFriend;
+function generateParticipants() {
+    database.remove();
+
+    // Nombres de los participantes
+    const names = ["Daniel", "Susana", "Mariana", "Agustina", "Gonzalo"];
+
+    names.forEach(name => {
+        let participant = new Participant(null, name, "", false, false);
+        save(participant);
+    })
 }
 
+
+function assignInvisibleFriends() {
+    refreshParticipants(() => {
+        participants.forEach(participant => {
+            if (participant.friend == '' || participant.friend == null) {
+                getInvisibleFriend(participant);
+            }
+        })
+    });
+}
+
+function generateAndAssignParticipants() {
+    generateParticipants();
+    assignInvisibleFriends();
+}
+
+function refreshParticipants(callback) {
+    database.once('value', function (snapshot) {
+        parseData(snapshot.val());
+        if (callback != null) {
+            callback();
+        }
+    });
+}
+
+// Functions to help in the development. Will be removed 
+// 
+//
+function showActions() {
+    let containerDisplay = getComputedStyle(document.getElementById("dataContainer")).display;
+    document.getElementById("dataContainer").style.display = containerDisplay == 'none' || containerDisplay == "" ? 'flex' : 'none';
+}
+
+function toggleElement(elementId) {
+    document.getElementById(elementId).style.opacity = document.getElementById(elementId).style.opacity == "" || document.getElementById(elementId).style.opacity == 1 ? 0 : 1;
+}
+
+function fillResultsList() {
+    database.once('value', function (snapshot) {
+        fillTable("resultsDataTable", snapshot.val());
+    });
+}
+
+function fillList(listId, json) {
+    let items = parseJsonToList(json);
+    //
+    let list = document.getElementById(listId);
+    list.innerHTML = "";
+
+    items.forEach(item => {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = `${item.name} - Friend: ${item.friend} - Selected: ${item.isSelected}`;
+        list.appendChild(listItem);
+    })
+}
+
+function fillTable(tableId, json) {
+    const items = parseJsonToList(json);
+    //
+    const table = document.getElementById(tableId);
+    //
+    items.forEach(item => {
+        let row = table.insertRow();
+        let nameColumn = row.insertCell(0);
+        nameColumn.innerHTML = item.name;
+        let friendColumn = row.insertCell(1);
+        friendColumn.innerHTML = item.friend;
+        //
+        if (item.friend != null && item.friend != "" && item.isSelected) {
+            row.classList.add('completed');
+        } else if (item.friend != null && item.friend != "" && !item.isSelected) {
+            row.classList.add('selected');
+        } else if (item.isSelected) {
+            row.classList.add('chosen');
+        }
+    });
+}
--- participant.js
+++ participant.js
@@ -1,10 +1,11 @@
 class Participant {
 
-    constructor(id, name, friend, isSelected) {
+    constructor(id, name, friend, isSelected, friendViewed) {
         this.id = id;
         this.name = name;
         this.friend = friend;  // != "" => participant already has a friend
         this.isSelected = isSelected; // true if this participant was selected by another one
+        this.friendViewed = friendViewed; // if participant already saw his friend
     }
 
 }
